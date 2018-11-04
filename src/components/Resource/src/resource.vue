2d1be94246d65d5876c8a4daf21b5351bff1b265
Santiago <sgobotta@gmail.com>
2018-11-04 13:19:08 -0300

    Resource: adds functionality to add dynamic routes for the different views

--- src/components/Resource/src/resource.vue
+++ src/components/Resource/src/resource.vue
@@ -4,12 +4,14 @@
 
 <script>
 import List from "../../List";
+import Show from "../../Show";
 import createCrudModule from "vuex-crud";
 
 export default {
   name: "Resource",
   props: {
-    name: String
+    name: String,
+    views: Array
   },
   data() {
     return {
@@ -19,21 +21,42 @@ export default {
   created: function() {
     let module = createCrudModule({
       resource: this.name,
-      urlRoot: "http://localhost:8080/api/" + this.name
+      customUrlFn(id) {
+        const rootUrl = 'http://localhost:8081/api/articles/' // TODO: usar una constante para http://localhost:8081 - santiago
+        return id ? `${rootUrl}${id}` : rootUrl
+      }
     });
     this.$store.registerModule(this.name, module);
   },
   methods: {
-    addRoute: function() {
+    addRoute: function(path, name) {
       const resourceName = "resources/addRoute";
-      this.$store.commit(resourceName, { path: this.fullRoute, name: this.name });
+      this.$store.commit(resourceName, { path, name });
     },
     loadRoutes() {
-      this.fullRoute = "/" + this.name + "/list";
-      this.addRoute();
-      this.$router.addRoutes([
-        { path: this.fullRoute, component: List, props: { name: this.name } }
-      ]);
+      this.fullRoute = "/" + this.name;
+      const path = this.fullRoute;
+      const hasShow = this.views.some(v => v === 'show');
+      const routes = [];
+      this.views.forEach((view) => {
+        switch(view) {
+          case "list":
+            this.addRoute(path, this.name);
+            routes.push({ path: path, name: `${this.name}/list`, component: List, props: { name: this.name, hasShow }});
+            break;
+          case "show":
+            // Es solo un ejemplo que agrega un link al Drawer para obtener el article con id '1'. Solamente la list debería agregarse por ahora. - santiago
+            // this.addRoute(`${path}/1`, this.name);
+
+            // Agrega una ruta para Ver un articulo
+            routes.push({ path: `${path}/:id`, name: `${this.name}/show`, component: Show, props: { name: this.name }});
+            break;
+          default:
+            break;
+        }
+      });
+
+      this.$router.addRoutes(routes);
     }
   },
   mounted: function() {
