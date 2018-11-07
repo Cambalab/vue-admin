d940f742bd07bb5fe58e35427d5afa750d1e2294
Santiago <sgobotta@gmail.com>
2018-11-07 00:00:23 -0300

    Creates a Create component for the resource component

---
+++ src/components/Create/Create.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <h1> {{name}} resource: create operation </h1>
+    <div>
+      <input v-model="article.title" placeholder="edit title" />
+      <input v-model="article.content" placeholder="edit content" />
+      <button v-on:click="submit">Save</button>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapState } from "vuex";
+
+export default {
+  name: "Create",
+
+  props: {
+    name: {
+      type: String,
+      default: null
+    }
+  },
+  data() {
+    return {
+      article: {
+        title: '',
+        content: ''
+      }
+    }
+  },
+  computed: {
+    ...mapState([
+      "route" // vuex-router-sync
+    ])
+  },
+
+  methods: {
+    submit() {
+      const resourceName = this.name + "/create";
+      const article = {
+        title: this.article.title,
+        content: this.article.content
+      }
+      return this.$store.dispatch(resourceName, { data: article });
+    }
+  },
+
+  created() {
+
+  }
+};
+</script>
---
+++ src/components/Create/index.js
@@ -0,0 +1,7 @@
+import Create from "./Create";
+
+Create.install = function(Vue) {
+  Vue.component(Create.name, Create);
+};
+
+export default Create;
