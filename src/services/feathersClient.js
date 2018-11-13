b9783b033a4071e50ea9f266fb14bbe0faf19014
Santiago <sgobotta@gmail.com>
2018-11-12 23:39:18 -0300

    Removes unnecesary feathers client

--- src/services/feathersClient.js
+++
@@ -1,6 +0,0 @@
-import feathers from "@feathersjs/client";
-
-const host = 'http://localhost:8080';
-
-export default feathers()
-    .configure(feathers.rest(host).fetch(window.fetch.bind(window)))
