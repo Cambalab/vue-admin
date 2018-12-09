14a7bff59399fef8c0aed7cc46319ae2f8486d68
Santiago <sgobotta@gmail.com>
2018-12-09 14:06:05 -0300

    Updated factory to return shortened paragraphs

--- tests/e2e/factory/index.js
+++ tests/e2e/factory/index.js
@@ -4,10 +4,13 @@ const ipsum = new Ipsum()
 
 export default {
   createArticle: ({ args } = {}) => {
+    // Shortens the paragraph
+    const title = ipsum.generateSentence()
+    const content = ipsum.generateParagraph().substring(0, 100)
     const _args = {
       id: 0,
-      title: ipsum.generateSentence(),
-      content: ipsum.generateParagraph()
+      title,
+      content
     }
     return Object.assign({}, args, _args)
   }
