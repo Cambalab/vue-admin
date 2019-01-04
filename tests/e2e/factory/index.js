def77dd2ee3114acbcbdf55092d615f34bede516
Estefi <estefi@camba.coop>
2019-01-04 11:25:57 -0300

    Parametrized the factory api url

--- tests/e2e/factory/index.js
+++ tests/e2e/factory/index.js
@@ -13,5 +13,11 @@ export default {
     }
     return Object.assign({}, _args, args)
   },
-  apiUrl: () => { return 'http://localhost:8080/api/articles/' }
+  apiUrl: ({ url, port, route }) => {
+    const address = {}
+    address.url  = url || 'http://localhost'
+    address.port = port || '8080'
+    address.route = route || ''
+    return `${address.url}:${address.port}/${address.route}`
+  }
 }
