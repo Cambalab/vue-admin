ec97329327222882be945b2827a7fe57c178035e
Santiago <sgobotta@gmail.com>
2019-01-06 12:47:39 -0300

    Refactored the factory utils and added a createMagazine function

--- tests/e2e/factory/index.js
+++ tests/e2e/factory/index.js
@@ -1,18 +1,29 @@
-const Ipsum = require("bavaria-ipsum");
-
-const ipsum = new Ipsum()
+import { ipsum, numbers } from './utils'
 
 export default {
   createArticle: (args = {}) => {
     // Shortens the paragraph
     const title = ipsum.generateSentence()
-    const content = ipsum.generateParagraph().substring(0, 100)
+    const content = ipsum.generateParagraph({ useStartingSentence: true })
     const _args = {
       title,
       content
     }
     return Object.assign({}, _args, args)
   },
+
+  createMagazine: (args = {}) => {
+    const name = ipsum.generateSentence()
+    const issue = `#${numbers.randomBetween(1, 500)}`
+    const publisher = ipsum.generateParagraph(1, { useStartingSentence: true })
+    const _args = {
+      name,
+      issue,
+      publisher
+    }
+    return Object.assign({}, _args, args)
+  },
+
   apiUrl: ({ url, port, route }) => {
     const address = {}
     address.url  = url || 'http://localhost'
---
+++ tests/e2e/factory/utils.js
@@ -0,0 +1,15 @@
+const Ipsum = require('bavaria-ipsum');
+
+export const ipsum = new Ipsum({
+  startSentence: 'Vue Admin aspera iaspis',
+  minSentenceWords: 2,
+  maxSentenceWords: 6,
+  minParagraphSentences: 1,
+  maxParagraphSentences: 3
+})
+
+export const numbers = {
+  randomBetween(min, max) {
+    return Math.floor(Math.random()*(max-min+1)+min)
+  }
+}
