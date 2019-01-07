8047d304a66ee89209fd8a39fa6df8dede237858
Santiago <sgobotta@gmail.com>
2019-01-06 21:47:26 -0300

    Adds a deprecation TODO in the e2e helpers module

--- tests/e2e/helpers/index.js
+++ tests/e2e/helpers/index.js
@@ -2,6 +2,7 @@
  * Cypress Helpers
  */
 
+// TODO: deprecate this file and use ../lib/helpers instead ^_^
  export default {
    queryElementByProp: ({ type = '', prop, value }) => `${type}[${prop}=${value}]`
  }
