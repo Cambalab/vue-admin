97ff03bea4ce849c38ca695808afd056288325f9
Santiago <sgobotta@gmail.com>
2019-01-06 21:47:57 -0300

    Refactors the Magazines Create test

--- tests/e2e/specs/magazines/create.spec.js
+++ tests/e2e/specs/magazines/create.spec.js
@@ -1,12 +1,15 @@
 const Factory = require('../../factory')
-const { queryElementByProp } = require('../../helpers')
 
-const UI_NAMES = require('../../../../src/constants/ui.element.names')
+const { InitEntityUtils } = require('../../lib/commands')
 
 describe('Magazines: Create Action Test', () => {
   const magazine = Factory.createMagazine()
+  const utils = InitEntityUtils({
+    resourceName: 'magazines',
+    view: 'create'
+  })
 
-  before('Visits the app root url', () => {
+  before('Visits the magazines create url', () => {
     cy.visit('/#/magazines/create')
   })
 
@@ -14,84 +17,39 @@ describe('Magazines: Create Action Test', () => {
     cy.url().should('include', 'magazines/create')
   })
 
-  it('The Name input is filled when a user types in', () => {
-    // Generates the name property value
-    const nameInputName = UI_NAMES.RESOURCE_VIEW_ELEMENT_FIELD.with({
-      resourceName: 'magazines',
-      view: 'create',
-      field: 'name'
-    })
-    // Generates the cypress query
-    const inputElement = queryElementByProp({
-      type: 'input',
-      prop: 'name',
-      value: nameInputName
-    })
-    // Gets the input element
-    const input = cy.get(inputElement)
-    // Types in the magazine name
+  it('The {Name} input is filled when a user types in', () => {
+    // Setup: Gets the 'name' input element
+    const input = utils.getInputBy({ field: 'name' })
+    // Excersice: Types in the magazine name
     input.type(magazine.name)
-    // Asserts the input contains the name
+    // Assertion: the input contains the magazine issue content
     input.should('have.value', magazine.name)
   })
 
-  it('The Issue input is filled when user types in', () => {
-    // Generates the name property value
-    const issueInputName = UI_NAMES.RESOURCE_VIEW_ELEMENT_FIELD.with({
-      resourceName: 'magazines',
-      view: 'create',
-      field: 'issue'
-    })
-    // Generates the cypress query
-    const inputElement = queryElementByProp({
-      type: 'input',
-      prop: 'name',
-      value: issueInputName
-    })
-    // Gets the input element
-    const input = cy.get(inputElement)
-    // Types in the magazine issue
+  it('The {Issue} input is filled when user types in', () => {
+    // Setup: Gets the 'issue' input element
+    const input = utils.getInputBy({ field: 'issue' })
+    // Excersice: Types in the magazine issue
     input.type(magazine.issue)
-    // Asserts the input contains the issue
+    // Assertion: the input contains the magazine issue content
     input.should('have.value', magazine.issue)
   })
 
-  it('The Publisher input is filled when user types in', () => {
-    // Generates the name property value
-    const publisherInputName = UI_NAMES.RESOURCE_VIEW_ELEMENT_FIELD.with({
-      resourceName: 'magazines',
-      view: 'create',
-      field: 'publisher'
-    })
-    // Generates the cypress query
-    const inputElement = queryElementByProp({
-      type: 'input',
-      prop: 'name',
-      value: publisherInputName
-    })
-    // Gets the input element
-    const input = cy.get(inputElement)
-    // Types in the magazine publisher
+  it('The {Publisher} input is filled when user types in', () => {
+    // Setup: Gets the 'publisher' input element
+    const input = utils.getInputBy({ field: 'publisher' })
+    // Excersice: Types in the magazine publisher
     input.type(magazine.publisher)
-    // Asserts the input contains the publisher
+    // Assertion: the input contains the magazine publisher content
     input.should('have.value', magazine.publisher)
   })
 
   it('A magazine is created when the user submits the form', () => {
-    // Generates the name property value
-    const submitButtonName = UI_NAMES.RESOURCE_CREATE_SUBMIT_BUTTON.with({
-      resourceName: 'magazines',
-      view: 'create'
-    })
-    // Generates the cypress query
-    const buttonElement = queryElementByProp({
-      type: 'button',
-      prop: 'name',
-      value: submitButtonName
-    })
-    // Gets the button element
-    cy.get(buttonElement).click()
-    // Asserts the router redirected to /magazines
+    // Setup: Gets the submit button element
+    const button = utils.getSubmitButton()
+    // Excersice: Creates a new magazine
+    button.click()
+    // Assertion: The router redirects to /magazines
     cy.url().should('include', '/magazines')
   })
 })
