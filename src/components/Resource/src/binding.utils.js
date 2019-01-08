b432a03912655b4531f8eef150127312d31fd053
Santiago <sgobotta@gmail.com>
2019-01-07 22:08:56 -0300

    Resource: now accepts a list component as an array or Object

--- src/components/Resource/src/binding.utils.js
+++ src/components/Resource/src/binding.utils.js
@@ -1,3 +1,4 @@
+import listUtils from '../../../store/utils/list.utils'
 import showUtils from '../../../store/utils/show.utils'
 import createUtils from '../../../store/utils/create.utils'
 import editUtils from '../../../store/utils/edit.utils'
@@ -21,23 +22,46 @@ export default ({
 
     list: ({ wrapper }) => {
       const name = `${resourceName}/list`
-      return list ? {
-        path: resourcePath,
-        name,
-        component: wrapper,
-        props: {
-          name: resourceName,
-          fields: list,
-          hasShow,
-          hasCreate,
-          resourceId: resourceIdName
+      if (list instanceof Array) {
+        // list should be a VA default component
+        return list ? {
+          path: resourcePath,
+          name,
+          component: wrapper,
+          props: {
+            name: resourceName,
+            fields: list,
+            hasShow,
+            hasCreate,
+            resourceId: resourceIdName
+          }
+        } : []
+      } else {
+        // list is a user's custom component
+        const utils = listUtils({
+          resourceName,
+          router,
+          store
+        })
+        return {
+          path: resourcePath,
+          name,
+          component: list,
+          props: {
+            // This could be refactored into a vue mixin, check #52 - @sgobotta
+            va: {
+              resourceName,
+              ...utils
+            }
+          }
         }
-      } : []
+      }
     },
 
     show: ({ wrapper }) => {
       const name = `${resourceName}/show`
       if (show instanceof Array) {
+        // show should be a VA default component
         return show ? {
           path: `${resourcePath}/show/:id`,
           name,
@@ -48,6 +72,7 @@ export default ({
           }
         } : []
       } else {
+        // show is a user's custom component
         const utils = showUtils({
           resourceName,
           router,
@@ -58,6 +83,7 @@ export default ({
           name,
           component: show,
           props: {
+            // This could be refactored into a vue mixin, check #52 - @sgobotta
             va: {
               resourceName,
               ...utils
@@ -71,6 +97,7 @@ export default ({
       const name = `${resourceName}/create`
       const redirectView = redirection.views.create
       if (create instanceof Array) {
+        // create should be a VA default component
         const redirect = { idField: resourceIdName, view: redirectView }
         return create ? {
           path: `${resourcePath}/create`,
@@ -83,6 +110,7 @@ export default ({
           }
         } : []
       } else {
+        // create is a user's custom component
         const utils = createUtils({
           redirectView,
           resourceIdName,
@@ -96,6 +124,7 @@ export default ({
           name,
           component: create,
           props: {
+            // This could be refactored into a vue mixin, check #52 - @sgobotta
             va: {
               resourceName,
               ...utils
@@ -109,6 +138,7 @@ export default ({
       const name = `${resourceName}/edit`
       const redirectView = redirection.views.edit
       if (edit instanceof Array) {
+        // edit should be a VA default component
         const redirect = { idField: resourceIdName, view: redirectView }
         return edit ? {
           path: `${resourcePath}/edit/:id`,
@@ -121,6 +151,7 @@ export default ({
           }
         } : []
       } else {
+        // edit is a user's custom component
         const utils = editUtils({
           redirectView,
           resourceIdName,
@@ -133,6 +164,7 @@ export default ({
           path: `${resourcePath}/edit/:id`,
           name,
           component: edit,
+          // This could be refactored into a vue mixin, check #52 - @sgobotta
           props: {
             va: {
               resourceName,
