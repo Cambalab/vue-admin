b2beb4b570da169afa4727d94dfa0438932245cf
Santiago <sgobotta@gmail.com>
2019-01-11 17:56:09 -0300

    Refactors the store testing utils

--- tests/e2e/factory/index.js
+++ tests/e2e/factory/index.js
@@ -1,34 +1,44 @@
 import { ipsum, numbers } from './utils'
+import { store } from './store'
+
+const createVuexCrudInitialState = () => {
+  return store.createVuexCrudInitialState()
+}
+
+const createArticle = (args = {}) => {
+  // Shortens the paragraph
+  const title = ipsum.generateSentence()
+  const content = ipsum.generateParagraph({ useStartingSentence: true })
+  const _args = {
+    title,
+    content
+  }
+  return Object.assign({}, _args, args)
+}
+
+const createMagazine = (args = {}) => {
+  const name = ipsum.generateSentence()
+  const issue = `#${numbers.randomBetween(1, 500)}`
+  const publisher = ipsum.generateParagraph(1, { useStartingSentence: true })
+  const _args = {
+    name,
+    issue,
+    publisher
+  }
+  return Object.assign({}, _args, args)
+}
+
+const apiUrl = ({ url, port, route }) => {
+  const address = {}
+  address.url  = url || 'http://localhost'
+  address.port = port || '8080'
+  address.route = route || ''
+  return `${address.url}:${address.port}/${address.route}`
+}
 
 export default {
-  createArticle: (args = {}) => {
-    // Shortens the paragraph
-    const title = ipsum.generateSentence()
-    const content = ipsum.generateParagraph({ useStartingSentence: true })
-    const _args = {
-      title,
-      content
-    }
-    return Object.assign({}, _args, args)
-  },
-
-  createMagazine: (args = {}) => {
-    const name = ipsum.generateSentence()
-    const issue = `#${numbers.randomBetween(1, 500)}`
-    const publisher = ipsum.generateParagraph(1, { useStartingSentence: true })
-    const _args = {
-      name,
-      issue,
-      publisher
-    }
-    return Object.assign({}, _args, args)
-  },
-
-  apiUrl: ({ url, port, route }) => {
-    const address = {}
-    address.url  = url || 'http://localhost'
-    address.port = port || '8080'
-    address.route = route || ''
-    return `${address.url}:${address.port}/${address.route}`
-  }
+  createVuexCrudInitialState,
+  createArticle,
+  createMagazine,
+  apiUrl
 }
