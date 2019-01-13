1dd2acd1aaa280f52eb5ce59e1ff7a4e07be4aae
Santiago <sgobotta@gmail.com>
2019-01-13 15:32:28 -0300

    Store: creates an initial state module

---
+++ tests/e2e/factory/store/initial.getters.js
@@ -0,0 +1,48 @@
+import { initialResourcesRoutes } from './common.utils'
+
+
+/**
+ * Annonymous Function - Creates a simualtion of initial vuex crud getters
+ *
+ * @return {Object} The expected Vuex Crud mocked getters
+ */
+export default () => {
+  // Initial vuex crud resources should be added here
+  const initialResources = [
+    'articles',
+    'magazines'
+  ]
+  // Vuex initial entities getters should be added here
+  const entitiesCrud = {
+    'entities/getEntity': {}
+  }
+  // Vuex initial resources getters should be added here
+  const resourcesGetters = {
+    'resources/all': initialResourcesRoutes(initialResources)
+  }
+
+  /**
+   * initResourcesCrud - Given a list of resources, creates mocked vuex crud
+   * methods for each of them
+   *
+   * @param {Array} resources An array of strings
+   *
+   * @return {Object} An object with mocked vuex crud methods
+   */
+  function initResourcesCrud(resources) {
+    const crud = {}
+    resources.forEach(resource => {
+      crud[`${resource}/byId`] = (id) => id,
+      crud[`${resource}/isError`] = false,
+      crud[`${resource}/isLoading`] = false,
+      crud[`${resource}/list`] = []
+    })
+    return crud
+  }
+
+  return {
+    ...initResourcesCrud(initialResources),
+    ...entitiesCrud,
+    ...resourcesGetters
+  }
+}
