2c177f1eea530ddef352b1fa55f880e0a75aee94
Santiago <sgobotta@gmail.com>
2019-01-13 15:34:50 -0300

    Store Getters test: implemented the spec file

--- tests/e2e/specs/store/getters.spec.js
+++ tests/e2e/specs/store/getters.spec.js
@@ -1,65 +1,58 @@
+import Factory from '../../factory'
+
 describe('Vuex Store Getters', () => {
-  const getStore = () => cy.window().its('app.$store')
+  const getStore = () => cy.getStore()
+  const initialGetters = {}
 
   before('Initialises the store', () => {
+    const _initialGetters = Factory.createInitialVuexStoreGetters()
+    Object.assign(initialGetters, _initialGetters)
     cy.visit('/#/')
     cy.reload()
   })
 
   it('Should have attributes on initialisation', () => {
-    const attribute = 'getters'
-    getStore().its(attribute).should('have.keys', [
-      'articles/byId',
-      'articles/isError',
-      'articles/isLoading',
-      'articles/list',
-      'magazines/byId',
-      'magazines/isError',
-      'magazines/isLoading',
-      'magazines/list',
-      'entities/getEntity',
-      'resources/all'
-    ])
+    getStore().its('getters').should('have.keys', Object.keys(initialGetters))
   })
 
   it('Attribute {resources/all} should be initialised', () => {
-    const attribute = 'getters.resources/all'
-    getStore().its(attribute).should('deep.equal', [
-      {
-        name: 'articles',
-        path: '/articles'
-      },
-      {
-        name: 'magazines',
-        path: '/magazines'
-      }
-    ])
+    const attribute = 'resources/all'
+    const getters = `getters.${attribute}`
+    getStore().its(getters).should('deep.equal', initialGetters[attribute])
   })
 
   it('{Articles} getters should be initialised', () => {
-    const attribute = 'getters'
-    getStore().its(attribute).should((getters) => {
-      assert.isFunction(getters['articles/byId'])
-      expect(getters['articles/isError']).to.equal(false)
-      expect(getters['articles/isLoading']).to.equal(false)
-      expect(getters['articles/list']).to.be.empty
-    })
+    const resource = 'articles'
+    gettersShouldBeInitialised(resource)
   })
 
   it('{Magazines} getters should be initialised', () => {
-    const attribute = 'getters'
-    getStore().its(attribute).should((getters) => {
-      assert.isFunction(getters['magazines/byId'])
-      expect(getters['magazines/isError']).to.equal(false)
-      expect(getters['magazines/isLoading']).to.equal(false)
-      expect(getters['articles/list']).to.be.empty
-    })
+    const resource = 'magazines'
+    gettersShouldBeInitialised(resource)
   })
 
   it('{Entities} getters should be initialised', () => {
-    const attribute = 'getters'
-    getStore().its(attribute).should((getters) => {
+    getStore().its('getters').should((getters) => {
       expect(getters['entities/getEntity']).to.be.empty
     })
   })
+
+  /**
+   * gettersShouldBeInitialised - Given a resource, asserts the current store
+   * getters are properly initialised
+   *
+   * @param {String} resource The name of a resource
+   */
+  function gettersShouldBeInitialised(resource) {
+    const byId = `${resource}/byId`
+    const isError = `${resource}/isError`
+    const isLoading = `${resource}/isLoading`
+    const list = `${resource}/list`
+    getStore().its('getters').should(getters => {
+      assert.isFunction(getters[byId])
+      expect(getters[isError]).to.equal(initialGetters[isError])
+      expect(getters[isLoading]).to.equal(initialGetters[isLoading])
+      expect(getters[list]).to.be.empty
+    })
+  }
 })
