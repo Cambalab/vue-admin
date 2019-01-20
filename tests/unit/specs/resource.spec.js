ac94d149f199495392eddcbc03357bacc904137c
Santiago <sgobotta@gmail.com>
2019-01-20 20:19:45 -0300

    Resource Tests: Adds negative tests for invalid properties

--- tests/unit/specs/resource.spec.js
+++ tests/unit/specs/resource.spec.js
@@ -1,13 +1,14 @@
 import Factory from '../factory'
-import resoureFixture from '../fixtures/resource/magazines'
-import Resource from '@/components/Resource'
+import resourceFixture from '../fixtures/resource/magazines'
+import Resource from '@components/Resource'
 import VueRouter from "vue-router"
 import Vuex from 'vuex'
 import { shallowMount, createLocalVue } from '@vue/test-utils'
+import ERROR_MESSAGES from '@constants/error.messages'
 
 describe('Resource.vue', () => {
   const subject = 'Resource'
-  const initialResources = [resoureFixture.props.name]
+  const initialResources = [resourceFixture.props.name]
   // Initialises the vue instance and Resource dependencies
   const localVue = createLocalVue()
   localVue.use(VueRouter)
@@ -36,12 +37,12 @@ describe('Resource.vue', () => {
     mocks = { $store: mockedStore, $router: mockedRouter }
     // Configures the subject props
     propsData = {
-      name: resoureFixture.props.name,
-      apiUrl: resoureFixture.props.apiUrl,
-      create: resoureFixture.props.create,
-      edit: resoureFixture.props.edit,
-      list: resoureFixture.props.list,
-      show: resoureFixture.props.show
+      name: resourceFixture.props.name,
+      apiUrl: resourceFixture.props.apiUrl,
+      create: resourceFixture.props.create,
+      edit: resourceFixture.props.edit,
+      list: resourceFixture.props.list,
+      show: resourceFixture.props.show
     }
     // Configures subject spy methods
     routerSpy = {
@@ -50,44 +51,49 @@ describe('Resource.vue', () => {
     storeSpy = {
       addRoute: jest.spyOn(mocks.$store, 'commit')
     }
-    // Mounts the subject instance
-    subjectWrapper = shallowMount(Resource, {
-      mocks,
-      propsData
-    })
   })
 
   it('should have default props', () => {
+    // Exercise: mounts the subject instance
+    mountSubject()
     const props = subjectWrapper.props()
     expect(subjectWrapper.name()).toMatch(subject)
-    expect(props.resourceIdName).toMatch(resoureFixture.props.resourceIdName)
-    expect(props.redirect).toMatchObject(resoureFixture.props.redirect)
-    expect(props.parseResponses).toMatchObject(resoureFixture.props.parseResponses)
+    expect(props.resourceIdName).toMatch(resourceFixture.props.resourceIdName)
+    expect(props.redirect).toMatchObject(resourceFixture.props.redirect)
+    expect(props.parseResponses).toMatchObject(resourceFixture.props.parseResponses)
   })
 
   it('should have non default props', () => {
+    // Exercise: mounts the subject instance
+    mountSubject()
     const props = subjectWrapper.props()
-    expect(props.name).toMatch(resoureFixture.props.name)
-    expect(props.create).toMatchObject(resoureFixture.props.create)
-    expect(props.edit).toMatchObject(resoureFixture.props.edit)
-    expect(props.list).toMatchObject(resoureFixture.props.list)
-    expect(props.show).toMatchObject(resoureFixture.props.show)
-    expect(props.apiUrl).toMatch(resoureFixture.props.apiUrl)
+    expect(props.name).toMatch(resourceFixture.props.name)
+    expect(props.create).toMatchObject(resourceFixture.props.create)
+    expect(props.edit).toMatchObject(resourceFixture.props.edit)
+    expect(props.list).toMatchObject(resourceFixture.props.list)
+    expect(props.show).toMatchObject(resourceFixture.props.show)
+    expect(props.apiUrl).toMatch(resourceFixture.props.apiUrl)
   })
 
   it('should call router addRoutes', () => {
+    // Exercise: mounts the subject instance
+    mountSubject()
     expect(routerSpy.addRoutes).toHaveBeenCalledTimes(1)
   })
 
   it('should call store commit', () => {
+    // Exercise: mounts the subject instance
+    mountSubject()
     const methodName = 'resources/addRoute'
-    const { storeMethods } = resoureFixture.methods
+    const { storeMethods } = resourceFixture.methods
     const { params } = storeMethods[methodName]
     expect(storeSpy.addRoute).toHaveBeenCalledTimes(1)
     expect(storeSpy.addRoute).toHaveBeenCalledWith(methodName, params)
   })
 
   it('should have the store initialised with {initialResources} getters', () => {
+    // Exercise: mounts the subject instance
+    mountSubject()
     const { getters } = mocks.$store
     const expectedGetters = dummyStore.getters
     const expectedGettersKeys = Object.keys(expectedGetters)
@@ -95,4 +101,42 @@ describe('Resource.vue', () => {
       expect(getters).toHaveProperty(expectedKey)
     })
   })
+
+  it('should throw error when the {name} property is missing', () => {
+    const prop = 'name'
+    shouldThrowOnMissingProp({ prop, subject })
+  })
+
+  it('should throw error when the {apiUrl} property is missing', () => {
+    const prop = 'apiUrl'
+    shouldThrowOnMissingProp({ prop, subject })
+  })
+
+  it('should throw an error when the {list} property is missing', () => {
+    const prop = 'list'
+    shouldThrowOnMissingProp({ prop, subject })
+  })
+
+
+/**
+ * Helper functions
+ */
+
+  function shouldThrowOnMissingProp({ prop, subject }) {
+    // Setup: deletes the list prop before mounting
+    delete propsData[prop]
+    const { UNDEFINED_PROPERTY } = ERROR_MESSAGES
+    const at = subject
+    const message = UNDEFINED_PROPERTY.with({ prop, at })
+    // Exercise: mounts the subject instance
+    expect(mountSubject).toThrowError(message)
+  }
+
+  // Mounts the component
+  function mountSubject() {
+    subjectWrapper = shallowMount(Resource, {
+      mocks,
+      propsData
+    })
+  }
 })
