dacfa83ce6b1e98179e0a53d8aa755206dfc3b7f
Santiago <sgobotta@gmail.com>
2019-01-20 20:15:28 -0300

    Creates a defaults file that defines the Resource component initialisation attributes:
    + A new alias is defined in vue.config.js and jest.config.js : @handlers

---
+++ jest.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  moduleFileExtensions: [
+    'js',
+    'jsx',
+    'json',
+    'vue'
+  ],
+  transform: {
+    '^.+\\.vue$': 'vue-jest',
+    '.+\\.(css|styl|less|sass|scss|svg|png|jpg|ttf|woff|woff2)$': 'jest-transform-stub',
+    '^.+\\.jsx?$': 'babel-jest'
+  },
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+    '@components(.*)$': '<rootDir>/src/components/$1',
+    '@constants(.*)$': '<rootDir>/src/constants/$1',
+    '@handlers(.*)$': '<rootDir>/src/handlers/$1',
+    '@router(.*)$': '<rootDir>/src/router/$1',
+    '@store(.*)$': '<rootDir>/src/store/$1'
+  },
+  snapshotSerializers: [
+    'jest-serializer-vue'
+  ],
+  testMatch: [
+    '**/tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx)'
+  ],
+  testURL: 'http://localhost/'
+}
