d02718ae0a64df78ede75f9cb391f1e371d8a687
Santiago <sgobotta@gmail.com>
2019-01-22 23:51:06 -0300

    Constants: error messages update
    + The error constants now use the templates to build error messages

---
+++ src/constants/error.messages.js
@@ -0,0 +1,21 @@
+import templates from '@templates'
+
+const buildMessage = templates('en.error')
+
+function parseErrorDetails(details) {
+  return details.map(detail => `\t${detail.message}`).join('\n')
+}
+
+export default {
+  UNDEFINED_PROPERTY: {
+    with: ({ prop, at }) => {
+      return buildMessage('UNDEFINED_PROPERTY', { prop, at })
+    }
+  },
+  INVALID_SCHEMA: {
+    with: ({ prop, at, details }) => {
+      const _details = parseErrorDetails(details)
+      return buildMessage('INVALID_SCHEMA', { prop, at, details: _details })
+    }
+  }
+}
