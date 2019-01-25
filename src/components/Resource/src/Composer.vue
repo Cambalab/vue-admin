06125889e9adee0cc3dc9b3b94bfeab5c127de3d
Santiago <sgobotta@gmail.com>
2019-01-25 17:06:27 -0300

    Implements a composer as a wrapper of the Resource component

---
+++ src/components/Resource/src/Composer.vue
@@ -0,0 +1,26 @@
+<script>
+import Resource from './Resource'
+
+export default {
+  functional: true,
+  render(createElement, context) {
+    const { props } = context
+    const slots = context.slots()
+    const isEmpty = Object.keys(slots).length === 0
+
+    // If the context has slots, we build a map with the name of the view and
+    // the associated component to it
+    if (!isEmpty) {
+      Object.keys(slots).reduce((prevValue, nextValue) => {
+        return Object.assign(prevValue, {
+          [nextValue]: prevValue[nextValue][0].data.attrs.component
+        })
+      }, slots)
+      Object.assign(props, slots)
+      return createElement(Resource, { props })
+    }
+    // The context has no slots, and it's passed directly to Resource
+    return createElement(Resource, context)
+  }
+}
+</script>
