feaa6a96ab576112e287958d8ffb80cc9e9bba9c
Santiago <sgobotta@gmail.com>
2019-01-25 17:07:21 -0300

    Implements a Composer as a wrapper of the List action component

---
+++ src/components/Actions/List/Composer.vue
@@ -0,0 +1,27 @@
+
+<script>
+import List from './List'
+
+export default {
+  name: 'ListComposer',
+  functional: true,
+  render(createElement, context) {
+    const { parent } = context
+    const { hasCreate, hasShow, $attrs } = parent
+    const { resourceName, resourceIdName } = $attrs
+
+    const reducedChildren = context.children.map(child => {
+      return { label: child.data.attrs.source, tag: child.tag }
+    })
+    const props = {
+      resourceName,
+      hasCreate,
+      hasShow,
+      resourceIdName,
+      fields: reducedChildren
+    }
+    return createElement(List, { props })
+  }
+}
+
+</script>
