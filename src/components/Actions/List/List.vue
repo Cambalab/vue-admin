7254377c519540ade5b911a01d895ecc776ced35
Santiago <sgobotta@gmail.com>
2019-01-25 17:08:28 -0300

    List: removes the ID header by default
    + Renames some prop names

--- src/components/Actions/List/List.vue
+++ src/components/Actions/List/List.vue
@@ -1,28 +1,28 @@
 <template>
   <v-card :name="UI_NAMES.RESOURCE_VIEW_CONTAINER.with({ resourceName, view })">
     <div class="text-xs-center d-flex right">
-        <router-link
-          :name="`${UI_NAMES.RESOURCE_CREATE_BUTTON.with({ resourceName })}`"
-          v-if="hasCreate"
-          :to="{ name: `${name}/create` }">
-          <v-tooltip bottom>
-            <v-btn
-              icon
-              absolute
-              right
-              color="success"
-              slot="activator"
-              style="top:20px;">
-              <i class="v-icon material-icons">{{UI_CONTENT.RESOURCE_CREATE_BUTTON}}</i>
-            </v-btn>
-            <span>Create {{name}}</span>
-          </v-tooltip>
-        </router-link>
+      <router-link
+        :name="`${UI_NAMES.RESOURCE_CREATE_BUTTON.with({ resourceName })}`"
+        v-if="hasCreate"
+        :to="{ name: `${resourceName}/create` }">
+        <v-tooltip bottom>
+          <v-btn
+            icon
+            absolute
+            right
+            color="success"
+            slot="activator"
+            style="top:20px;">
+            <i class="v-icon material-icons">{{UI_CONTENT.RESOURCE_CREATE_BUTTON}}</i>
+          </v-btn>
+          <span>Create {{resourceName}}</span>
+        </v-tooltip>
+      </router-link>
     </div>
     <v-card-title
       :name="`${UI_NAMES.RESOURCE_VIEW_CONTAINER_TITLE.with({ resourceName, view })}`"
       primary-title>
-      <h3 class="headline mb-0 text-capitalize">{{name}}</h3>
+      <h3 class="headline mb-0 text-capitalize">{{resourceName}}</h3>
     </v-card-title>
 
     <v-data-table
@@ -33,40 +33,40 @@
         slot="items"
         slot-scope="props"
         >
-        <td>
-          <router-link
-            :name="`${UI_NAMES.RESOURCE_VIEW_ELEMENT_FIELD.with({ resourceName, view, field: resourceId, index: props.index })}`"
-            v-if="hasShow"
-            :to="{ name: `${name}/show`, params: { id: props.item[resourceId] } }">
-            {{ props.item[resourceId] }}
-          </router-link>
-          <span
-            :name="`${UI_NAMES.RESOURCE_VIEW_ELEMENT_FIELD.with({ resourceName, view, field: resourceId, index: props.index })}`"
-            v-else>
-            {{ props.item[resourceId] }}
-          </span>
-        </td>
         <td class="text-xs-left"
           v-for="field in fields"
           :key="key(label(field))"
           >
-          <component
-            :name="`${UI_NAMES.RESOURCE_VIEW_ELEMENT_FIELD.with({ resourceName, view, field: label(field), index: props.index })}`"
-            :is="type(field.type)"
-            v-bind:content="props.item[label(field)]"
-            v-bind="args(field)">
-          </component>
+          <router-link
+            :name="`${UI_NAMES.RESOURCE_VIEW_ELEMENT_FIELD.with({ resourceName, view, field: resourceIdName, index: props.index })}`"
+            v-if="label(field) === resourceIdName && hasShow"
+            :to="{ name: `${resourceName}/show`, params: { id: props.item[resourceIdName] } }">
+            <component
+              :name="`${UI_NAMES.RESOURCE_VIEW_ELEMENT_FIELD.with({ resourceName, view, field: label(field), index: props.index })}`"
+              :is="type(field.type)"
+              v-bind:content="props.item[label(field)]"
+              v-bind="args(field)">
+            </component>
+          </router-link>
+          <span v-else>
+            <component
+              :name="`${UI_NAMES.RESOURCE_VIEW_ELEMENT_FIELD.with({ resourceName, view, field: label(field), index: props.index })}`"
+              :is="type(field.type)"
+              v-bind:content="props.item[label(field)]"
+              v-bind="args(field)">
+            </component>
+          </span>
         </td>
         <td>
           <EditButton
-            :resourceId="props.item[resourceId]"
-            :resourceName="name">
+            :resourceId="props.item[resourceIdName]"
+            :resourceName="resourceName">
           </EditButton>
         </td>
         <td class="text-xs-right">
           <Delete
-            :resourceId="props.item[resourceId]"
-            :resourceName="name">
+            :resourceId="props.item[resourceIdName]"
+            :resourceName="resourceName">
           </Delete>
         </td>
       </template>
@@ -84,9 +84,8 @@ import { EditButton, Delete } from "../../Actions";
 
 export default {
   name: "List",
-
   props: {
-    name: {
+    resourceName: {
       type: String,
       default: null
     },
@@ -99,7 +98,7 @@ export default {
     fields: {
       type: Array
     },
-    resourceId:{
+    resourceIdName:{
       type: String
     }
   },
@@ -111,19 +110,8 @@ export default {
     }
   },
   computed: {
-    resourceName: function () {
-      return this.name
-    },
     headers: function () {
-      let newHeaders = [
-        {
-          text: "ID",
-          align: 'left',
-          sortable: true,
-          width: 10,
-          value: this.resourceId
-        }
-      ];
+      const newHeaders = []
       this.fields.forEach((field) => {
         newHeaders.push({
           text: this.label(field),
@@ -147,10 +135,9 @@ export default {
       return newHeaders;
     },
     resourceList: function() {
-      const resourceName = this.name + "/list";
+      const resourceName = this.resourceName + "/list";
       return this.$store.getters[resourceName];
     },
-
     ...mapState([
       "route" // vuex-router-sync
     ])
@@ -165,7 +152,7 @@ export default {
 
   methods: {
     fetchResource: function() {
-      const resourceName = this.name + "/fetchList";
+      const resourceName = this.resourceName + "/fetchList";
       return this.$store.dispatch(resourceName);
     },
 
@@ -178,7 +165,7 @@ export default {
     },
 
     key(label) {
-      return `${this.name}_${label}`
+      return `${this.resourceName}_${label}`
     },
 
     label(field) {
@@ -189,7 +176,6 @@ export default {
       const args = typeof(field) === 'string' ? { 'label': field } : field
       return args
     }
-
   },
 
   watch: {
