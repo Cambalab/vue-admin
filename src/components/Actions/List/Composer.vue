ad82f4d2c323ba80b74985b135b892f31199ff42
Santiago <sgobotta@gmail.com>
2019-01-27 17:26:55 -0300

    Updates the List composer to treat List as a parent of views or a component with an array of elements as props

--- src/components/Actions/List/Composer.vue
+++ src/components/Actions/List/Composer.vue
@@ -1,4 +1,3 @@
-
 <script>
 import List from './List'
 
@@ -6,22 +5,27 @@ export default {
   name: 'ListComposer',
   functional: true,
   render(createElement, context) {
-    const { parent } = context
-    const { hasCreate, hasShow, $attrs } = parent
-    const { resourceName, resourceIdName } = $attrs
+    // If List is being used in a user template it should contain View
+    // children, then they are processed and passed as props to the List
+    if (context.children) {
+      const { parent } = context
+      const { hasCreate, hasShow, $attrs } = parent
+      const { resourceName, resourceIdName } = $attrs
 
-    const reducedChildren = context.children.map(child => {
-      return { label: child.data.attrs.source, tag: child.tag }
-    })
-    const props = {
-      resourceName,
-      hasCreate,
-      hasShow,
-      resourceIdName,
-      fields: reducedChildren
+      const reducedChildren = context.children.map(child => {
+        return { label: child.data.attrs.source, tag: child.tag }
+      })
+      const props = {
+        resourceName,
+        hasCreate,
+        hasShow,
+        resourceIdName,
+        fields: reducedChildren
+      }
+      return createElement(List, { props })
     }
-    return createElement(List, { props })
+    // List is already being instanced by Resource as an Array
+    return createElement(List, context)
   }
 }
-
 </script>
