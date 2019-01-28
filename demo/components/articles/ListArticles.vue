c12ff5b9e3f85d2701b7dba1143c03cab4893a5e
Santiago <sgobotta@gmail.com>
2019-01-27 21:23:18 -0300

    App demo: renamed usage for the List component

--- demo/components/articles/ListArticles.vue
+++ demo/components/articles/ListArticles.vue
@@ -1,18 +1,18 @@
 <template>
-  <ListWrapper>
+  <List>
     <p source="id" :sortable="true" headerText="ID" alignHeader="left" alignContent="left" />
     <h3 source="title" :sortable="true" headerText="Title" alignHeader="center" alignContent="left" />
     <p source="content" :sortable="true" headerText="Content" alignHeader="center" alignContent="right" />
-  </ListWrapper>
+  </List>
 </template>
 
 <script>
-import ListWrapper from '@components/Actions/List'
+import List from '@components/Actions/List'
 
 export default {
   name: 'ListArticles',
   components: {
-    ListWrapper
+    List
   }
 }
 </script>
