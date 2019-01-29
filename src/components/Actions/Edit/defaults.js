7576c8133cceb82cd553d3e678649704252d173f
Santiago <sgobotta@gmail.com>
2019-01-29 19:30:05 -0300

    Creates defaults modules for the List, Show, Create and Edit views

---
+++ src/components/Actions/Edit/defaults.js
@@ -0,0 +1,25 @@
+
+/**
+ * Defaults - Default attributes for the Edit view
+ *
+ * @return {Object} An object containing props and methods
+ */
+export default () => {
+  /**
+   * Edit View default composer options
+   */
+   const composer = {
+     parentPropKeys: [
+       'resourceName',
+       'va'
+     ],
+     childrenAdapter: {
+       placeHolder: 'placeHolder',
+       source: 'label'
+     }
+   }
+
+  return {
+    composer
+  }
+}
