51a43967714d599ab4b45d9b75b92936639c7956
Santiago <sgobotta@gmail.com>
2019-03-17 12:54:51 -0300

    Updates Cypress theme configuration

--- tests/e2e/support/index.js
+++ tests/e2e/support/index.js
@@ -13,9 +13,16 @@
 // https://on.cypress.io/configuration
 // ***********************************************************
 
-// Import commands.js using ES2015 syntax:
+import moment from 'moment'
 import './commands'
-import 'cypress-dark/src/halloween'
 
-// Alternatively you can use CommonJS syntax:
-// require('./commands')
+function isOctober() {
+  const today            = moment()
+  const year             = today.year()
+  const format           = 'MM-DD-YYYY'
+  const octoberStartDate = moment(`10-01-${year}`, format)
+  const octoberEndDate   = moment(`10-31-${year}`, format)
+  return today.isBetween(octoberStartDate, octoberEndDate)
+}
+
+isOctober() ? require('cypress-dark/src/halloween') : require('cypress-dark')
