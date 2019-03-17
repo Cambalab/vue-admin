93cfbf3a98660a8c3a63de3c0ae540b79db2bb78
Santiago <sgobotta@gmail.com>
2019-03-17 18:31:42 -0300

    Implements a very basic adapter for axios with an AUTH_LOGIN_REQUEST case

---
+++ demo/va-auth-adapter/axios.adapter.js
@@ -0,0 +1,48 @@
+import AuthTypes from '@va-auth/types'
+
+export default (client, options = {}) => {
+  return (type, params) => {
+    const {
+      AUTH_LOGIN_REQUEST,
+    } = AuthTypes
+
+    const {
+      authUrl,
+      storageKey,
+      userFields,
+    } = Object.assign({
+      storageKey: 'token',
+      userFields: { username: 'username', password: 'password' },
+    }, options);
+
+    switch (type) {
+      case AUTH_LOGIN_REQUEST:
+        return new Promise((resolve, reject) => {
+          const headers = {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            [userFields.username]: params.username,
+            [userFields.password]: params.password
+          }
+          const method = 'post'
+          const url = authUrl
+
+          client({ url, headers, method })
+            .then(response => {
+              const { data } = response
+              const { [storageKey]: token, user } = data
+              // something more secure maybe?
+              localStorage.setItem(storageKey, token)
+              client.defaults.headers.common['Authorization'] = token
+              resolve(user)
+            })
+            .catch(error => {
+              localStorage.removeItem(storageKey)
+              reject(error)
+            })
+        })
+
+      default:
+        return Promise.reject(`Unsupported @va-auth action type: ${type}`);
+    }
+  }
+}
