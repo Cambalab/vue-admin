789d85495dab3a9e7dd451c90c4b14b3de26f29b
sgobotta <santiago@camba.coop>
2019-08-04 01:10:02 -0300

    Vuetify plugin: Adds mdi icons to the Vuetify instance

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,7 +1,12 @@
 import Vue from 'vue'
 import Vuetify from 'vuetify/lib'
+import '@mdi/font/css/materialdesignicons.css'
 import 'vuetify/dist/vuetify.min.css'
 
 Vue.use(Vuetify)
 
-export default new Vuetify()
+export default new Vuetify({
+  icons: {
+    iconfont: 'mdi',
+  },
+})
