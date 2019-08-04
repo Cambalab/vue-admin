ea014fa1814949d763135137649066e360a125c0
sgobotta <santiago@camba.coop>
2019-08-04 01:37:49 -0300

    List component: updates to the new vuetify data table
    + updates slot prop to v-slot
    + replaces edit and delete columns for actions column

--- src/components/Actions/List/List.vue
+++ src/components/Actions/List/List.vue
@@ -1,83 +1,90 @@
 <template>
   <v-card :name="UI_NAMES.RESOURCE_VIEW_CONTAINER.with({ resourceName, view })">
     <Spinner :spin="isLoading"></Spinner>
-    <div class="text-xs-center d-flex right">
-      <router-link
-        :name="`${UI_NAMES.RESOURCE_CREATE_BUTTON.with({ resourceName })}`"
-        v-if="hasCreate"
-        :to="{ name: `${resourceName}/create` }">
-        <v-tooltip bottom>
-          <v-btn
-            icon
-            absolute
-            right
-            color="success"
-            slot="activator"
-            style="top:20px;">
-            <i class="v-icon material-icons">{{UI_CONTENT.RESOURCE_CREATE_BUTTON}}</i>
-          </v-btn>
-          <span>Create {{resourceName}}</span>
-        </v-tooltip>
-      </router-link>
-    </div>
-    <v-card-title
-      :name="`${UI_NAMES.RESOURCE_VIEW_CONTAINER_TITLE.with({ resourceName, view })}`"
-      primary-title>
-      <h3 class="headline mb-0 text-capitalize">{{resourceName}}</h3>
-    </v-card-title>
 
     <v-data-table
       :headers="headers"
       :items="resourceList"
       class="elevation-1"
-      disable-initial-sort
-      :pagination.sync="pagination"
-      >
-      <template
-        slot="items"
-        slot-scope="props"
-        >
-        <td class="text-xs-left"
-          v-for="field in fields"
-          :key="key(label(field))"
-          :name="`${UI_NAMES.RESOURCE_VIEW_ELEMENT_FIELD.with({ resourceName, view, field: label(field), index: props.index })}`"
-          >
-          <router-link
-            :name="`${UI_NAMES.RESOURCE_VIEW_ELEMENT_FIELD.with({ resourceName, view, field: resourceIdName, index: props.index })}`"
-            v-if="label(field) === resourceIdName && hasShow"
-            :to="{ name: `${resourceName}/show`, params: { id: props.item[resourceIdName] } }">
-            <component
-              :name="`${UI_NAMES.RESOURCE_VIEW_ELEMENT_FIELD.with({ resourceName, view, field: label(field), index: props.index })}`"
-              :is="type(field.type)"
-              v-bind:value="props.item[label(field)]"
-              v-bind="args(field)">
-            </component>
-          </router-link>
-          <span v-else>
-            <component
-              :name="`${UI_NAMES.RESOURCE_VIEW_ELEMENT_FIELD.with({ resourceName, view, field: label(field), index: props.index })}`"
-              :is="type(field.type)"
-              v-bind:value="props.item[label(field)]"
-              v-bind="args(field)">
-            </component>
-          </span>
-        </td>
-        <td v-if="hasEdit">
-          <EditButton
-            :resourceId="props.item[resourceIdName]"
-            :resourceName="resourceName"
-            :index="props.index">
-          </EditButton>
-        </td>
-        <td>
-          <Delete
-            :resourceId="props.item[resourceIdName]"
-            :resourceName="resourceName"
-            :index="props.index"
-            >
-          </Delete>
-        </td>
+      :options.sync="options"
+    >
+
+      <template v-slot:top>
+        <v-toolbar flat color="white">
+          <v-toolbar-title :name="`${UI_NAMES.RESOURCE_VIEW_CONTAINER_TITLE.with({ resourceName, view })}`">
+            <h3 class="headline mb-0 text-capitalize">{{resourceName}}</h3>
+          </v-toolbar-title>
+          <v-divider
+            class="mx-4"
+            inset
+            vertical
+          />
+          <v-spacer/>
+          <div class="text-xs-center d-flex right">
+            <router-link
+              :name="`${UI_NAMES.RESOURCE_CREATE_BUTTON.with({ resourceName })}`"
+              v-if="hasCreate"
+              :to="{ name: `${resourceName}/create` }">
+              <v-tooltip bottom>
+                <template v-slot:activator="{ on }">
+                  <v-btn icon v-on="on">
+                    <v-icon color="teal">
+                      {{UI_CONTENT.RESOURCE_CREATE_BUTTON}}
+                    </v-icon>
+                  </v-btn>
+                </template>
+                <span>Create</span>
+              </v-tooltip>
+            </router-link>
+          </div>
+        </v-toolbar>
       </template>
+
+      <template v-slot:body="{ items }">
+        <tbody>
+           <tr v-for="(item, index) in items">
+             <td class="text-xs-left"
+               v-for="field in fields"
+               :key="key(label(field))"
+               :name="UI_NAMES.RESOURCE_VIEW_ELEMENT_FIELD.with({ resourceName, view, field: label(field), index })"
+               >
+               <router-link
+                 :name="UI_NAMES.RESOURCE_VIEW_ELEMENT_FIELD.with({ resourceName, view, field: resourceIdName, index })"
+                 v-if="label(field) === resourceIdName && hasShow"
+                 :to="{ name: `${resourceName}/show`, params: { id: item[resourceIdName] } }"
+               >
+                 <component
+                   :name="UI_NAMES.RESOURCE_VIEW_ELEMENT_FIELD.with({ resourceName, view, field: label(field), index })"
+                   :is="type(field.type)"
+                   v-bind:value="item[label(field)]"
+                   v-bind="args(field)"
+                 />
+               </router-link>
+               <span v-else>
+                 <component
+                   :name="UI_NAMES.RESOURCE_VIEW_ELEMENT_FIELD.with({ resourceName, view, field: label(field), index })"
+                   :is="type(field.type)"
+                   v-bind:value="item[label(field)]"
+                   v-bind="args(field)"
+                 />
+               </span>
+             </td>
+             <td>
+               <EditButton
+                 :resourceId="item[resourceIdName]"
+                 :resourceName="resourceName"
+                 :index="index"
+               />
+               <Delete
+                 :resourceId="item[resourceIdName]"
+                 :resourceName="resourceName"
+                 :index="index"
+               />
+             </td>
+           </tr>
+        </tbody>
+      </template>
+
     </v-data-table>
   </v-card>
 </template>
@@ -124,9 +131,11 @@ export default {
       view: 'list',
       UI_CONTENT,
       UI_NAMES,
-      pagination: {
-        page: 1,
-        rowsPerPage
+      options: {
+        pagination: {
+          page: 1,
+          itemsPerPage: rowsPerPage
+        }
       }
     }
   },
@@ -138,20 +147,16 @@ export default {
           text: field.headerText || this.label(field),
           align: field.alignHeader || 'left',
           sortable: field.sortable || false,
-          value: this.label(field)
+          value: this.label(field),
+          width: field.label === 'id' ? '70px' : ''
         })
       })
-      this.hasEdit && newHeaders.push({
-        text: "Edit",
-        align: 'center',
-        sortable: false,
-        width: 10
-      });
       newHeaders.push({
-        text: "Delete",
+        text: "Actions",
         align: 'center',
         sortable: false,
-        width: 10
+        value: 'action',
+        width: '150px'
       });
       return newHeaders;
     },
