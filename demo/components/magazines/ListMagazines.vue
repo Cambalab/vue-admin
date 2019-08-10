fd27cf3de14b87295c6ca37b4d309afb010b6a28
sgobotta <santiago@camba.coop>
2019-08-10 20:30:21 -0300

    Demo Magazines: updates table to satisfy vuetify 2 requirements

--- demo/components/magazines/ListMagazines.vue
+++ demo/components/magazines/ListMagazines.vue
@@ -1,69 +1,30 @@
 <template>
-  <v-card>
+  <v-card class="extra-margin">
 
     <v-img
       src="banner.png"
       aspect-ratio="4"
     />
 
-    <v-card-title primary-title>
-      <div>
-        <h3 class="headline mb-2">This is a Custom List Form</h3>
-        <p>
-          Although we provide default components for List views, Vue Admin
-          ships with a <i>kind of injected</i> set of functions for those
-          components declared in <b>Resource</b> as a view, that can be used
-          for fetching a list of a resource, getting a single element, etc.
-        </p>
-        <p>
-          The Vuex store is the middleware where data is saved until a
-          submit action is triggered. This seems to be the easiest way to
-          save data and call actions from <b>any</b> custom component, such
-          as inputs, textfields, buttons, etc...
-        </p>
-        <p>
-          This is one of the two proposals when we thought about user
-          customization:
-          <ul>
-            <li>
-              letting a user build their own components with any UI
-              frameworkand provide a simple API for updating and storing
-            </li>
-            <li>
-              build our own UI components with a single framework (possibly
-              Vuetify) and expose them to the user in the form of Buttons,
-              Inputs, TextFields, DataTables, ..., as fixed templates.
-            </li>
-          </ul>
-        </p>
-      </div>
-    </v-card-title>
-
-    <v-toolbar flat color="white">
-      <v-toolbar-title>My Magazines List</v-toolbar-title>
-      <v-divider
-        class="mx-2"
-        inset
-        vertical
-      />
-      <v-spacer></v-spacer>
-      <div max-width="500px">
-        <router-link
-          :to="{ name: `magazines/create` }">
-          <v-btn color="primary" dark class="mb-2">New Magazine</v-btn>
-        </router-link>
-      </div>
-    </v-toolbar>
-
     <v-data-table
       :headers="headers"
       :items="desserts"
       class="elevation-1"
-      disable-initial-sort
-      :pagination.sync="pagination"
+      :footer-props="footerProps"
+      :options.sync="options"
       :dark="true"
-      rows-per-page-text="Magazines per page"
     >
+
+      <template v-slot:top>
+        <v-toolbar dark flat>
+          <v-toolbar-title>Magazines</v-toolbar-title>
+          <v-spacer />
+          <router-link :to="{ name: `magazines/create` }" class="no-decoration">
+            <v-btn color="orange" dark class="mb-2">New Magazine</v-btn>
+          </router-link>
+        </v-toolbar>
+      </template>
+
       <template slot="no-data">
         <v-alert :value="true" color="warning" icon="warning">
           Sorry, nothing to display here :(
@@ -110,9 +71,13 @@
         nameRowName: (index) => buildName('name', index),
         issueRowName: (index) => buildName('issue', index),
         publisherRowName: (index) => buildName('publisher', index),
-        pagination: {
-          page: 1,
-          rowsPerPage
+        footerProps: {
+          itemsPerPageText: 'Magazines per page'
+        },
+        options: {
+          pagination: {
+            itemsPerPage: rowsPerPage
+          }
         }
       }
     },
@@ -134,3 +99,13 @@
     }
   }
 </script>
+
+<style>
+.no-decoration {
+  text-decoration: none
+}
+
+.extra-margin {
+  margin-bottom: 10px
+}
+</style>
