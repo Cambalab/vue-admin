9dee7bd7995f2369b2dc88d928467e570b2189ac
sgobotta <santiago@camba.coop>
2019-08-10 17:53:44 -0300

    Show: refactors names usage

--- src/components/Actions/Show/Show.vue
+++ src/components/Actions/Show/Show.vue
@@ -1,45 +1,44 @@
 <template>
-  <v-card :name="`${UI_NAMES.RESOURCE_VIEW_CONTAINER.with({ resourceName, view })}`">
+  <v-card :name="names.viewContainer">
     <Spinner :spin="isLoading"></Spinner>
-    <v-toolbar flat color="white">
-      <v-spacer/>
+    <v-toolbar
+      flat
+      color="white"
+      :name="names.viewActionsContainer"
+    >
+      <v-spacer />
       <EditButton
         :resourceId="$route.params.id"
-        :resourceName="resourceName">
-      </EditButton>
+        :resourceName="resourceName"
+      />
       <Delete
         :resourceId="$route.params.id"
-        :resourceName="resourceName">
-      </Delete>
+        :resourceName="resourceName"
+      />
     </v-toolbar>
-    <div class="text-xs-center d-flex right" :name="`${UI_NAMES.RESOURCE_VIEW_ACTIONS_CONTAINER.with({ resourceName, view })}`">
-
-    </div>
-    <v-card-text :name="`${UI_NAMES.RESOURCE_VIEW_CONTAINER_FIELDS.with({ resourceName, view })}`">
+    <v-card-text :name="names.containerFields">
       <component
-        :name="componentName(field)"
+        :name="names.containerField(label(field))"
         v-for="field in fields"
         v-if="resourceShow !== undefined"
-        :key="key(label(field))"
-        :is="type(field.type)"
+        :key="names.containerField(label(field))"
+        :is="type(field)"
         v-bind:value="resourceShow[label(field)]"
-        v-bind="args(field)">
-      </component>
+        v-bind="args(field)"
+      />
     </v-card-text>
   </v-card>
 </template>
 
-
 <script>
 import UI_CONTENT from '@constants/ui.content.default'
 import UI_NAMES from '@constants/ui.element.names'
-import { mapState } from "vuex";
+import { mapState } from "vuex"
 import { Input, TextField, Spinner } from "../../UiComponents"
-import { EditButton, Delete } from "../../Actions";
+import { EditButton, Delete } from "../../Actions"
 
 export default {
   name: "Show",
-
   props: {
     resourceName: {
       type: String,
@@ -54,70 +53,70 @@ export default {
       required: true
     }
   },
-
+  components: {
+    Input,
+    TextField,
+    Delete,
+    EditButton,
+    Spinner
+  },
   data() {
+    const resourceName = this.resourceName
+    const view = 'show'
+    const names = {
+      containerField: (field) => UI_NAMES.RESOURCE_VIEW_CONTAINER_FIELD.with({
+        resourceName,
+        view,
+        field
+      }),
+      containerFields: UI_NAMES.RESOURCE_VIEW_CONTAINER_FIELDS.with({
+        resourceName,
+        view
+      }),
+      viewActionsContainer: UI_NAMES.RESOURCE_VIEW_ACTIONS_CONTAINER.with({
+        resourceName,
+        view
+      }),
+      viewContainer: UI_NAMES.RESOURCE_VIEW_CONTAINER.with({
+        resourceName,
+        view
+      })
+    }
     return {
-      view: 'show',
-      UI_CONTENT,
-      UI_NAMES
+      names
     }
   },
-
   computed: {
     resourceShow: function() {
       return this.va.getEntity()
     },
-
     ...mapState([
       "route" // vuex-router-sync
     ]),
-
     isLoading() {
       return this.$store.getters['requests/isLoading']
     }
   },
-
-  components: {
-    Input: Input,
-    TextField: TextField,
-    Delete: Delete,
-    EditButton: EditButton,
-    Spinner
-  },
-
   methods: {
     fetchData() {
       return this.va.fetchEntity()
     },
-
-    type(type) {
-      return type || 'TextField'
+    type(field) {
+      return field.field || 'TextField'
     },
-
-    key(label) {
-      return `${this.resourceName}_${label}`
-    },
-
     label(field) {
       return field.label || field
     },
-
     args(field) {
       const args = typeof(field) === 'string' ? { 'label': field } : field
       return args
-    },
-
-    componentName(field) {
-      return UI_NAMES.RESOURCE_VIEW_CONTAINER_FIELD.with({ resourceName: this.resourceName, view: this.view, field: this.label(field) })
     }
   },
-
   watch: {
     $route: "fetchData"
   },
-
   created() {
-    this.fetchData();
+    this.fetchData()
   }
-};
+}
 </script>
