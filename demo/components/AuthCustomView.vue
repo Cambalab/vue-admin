90b23c86e7d067b348ac3e38300a6dd4edb4facb
sgobotta <santiago@camba.coop>
2019-08-17 00:51:15 -0300

    Custom Auth View: adds e-mail validation

--- demo/components/AuthCustomView.vue
+++ demo/components/AuthCustomView.vue
@@ -88,7 +88,8 @@ export default {
       password: '',
       valid: false,
       usernameRules: [
-        v => !!v || CUSTOM_AUTH_ALERT_USERNAME_REQUIRED
+        v => !!v || CUSTOM_AUTH_ALERT_USERNAME_REQUIRED,
+        v =>  /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/.test(v) || AUTH_ALERT_INVALID_EMAIL
       ],
       passwordRules: [
         v => !!v || CUSTOM_AUTH_ALERT_PASSWORD_REQUIRED,
