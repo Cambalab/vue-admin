281aea0ea933439316a48eb9d0eeb9c09fe7e5f7
sgobotta <santiago@camba.coop>
2019-08-24 21:10:18 -0300

    Creates an Authenticated component:
    + creates the resources module
    + creates the unauthorized route
    + renders the app layout passing the va props with login and user functions, and slots as children

---
+++ src/components/Admin/src/Authenticated.vue
@@ -0,0 +1,63 @@
+<script>
+import AuthActionTypes from '@va-auth/types'
+import Core from '@components/Core'
+import resourceModule from '@store/modules/resource'
+
+export default {
+  name: 'Authenticated',
+  props: {
+    layout: {
+      type: Object,
+      required: true
+    },
+    title: {
+      type: String,
+      required: true
+    },
+    sidebar: {
+      type: Object,
+      required: true
+    },
+    unauthorized: {
+      type: Object,
+      required: true
+    }
+  },
+  beforeCreate() {
+    this.$store.registerModule('resources', resourceModule)
+  },
+  created() {
+    const unauthorizedRoute = {
+      path: '/unauthorized',
+      name: 'unauthorized',
+      component: this.unauthorized
+    }
+    this.$router.addRoutes([unauthorizedRoute])
+  },
+  methods: {
+    logout: function () {
+      const { namespace, AUTH_LOGOUT_REQUEST } = AuthActionTypes
+      this.$store.dispatch(`${namespace}/${AUTH_LOGOUT_REQUEST}`)
+    },
+    getUser: function () {
+      const { namespace } = AuthActionTypes
+      return this.$store.getters[`${namespace}/getUser`]
+    }
+  },
+  render: function (createElement, context) {
+    const props = {
+      layout: this.layout,
+      title: this.title,
+      sidebar: this.sidebar,
+      unauthorized: this.unauthorized,
+      va: {
+        getUser: this.getUser,
+        logout: this.logout
+      }
+    }
+
+    return createElement(Core, { props }, this.$slots.default)
+  }
+}
+
+</script>
