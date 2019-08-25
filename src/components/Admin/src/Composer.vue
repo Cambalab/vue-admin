2a9ec0d3e3699a5f238d9e54d319fd06e0108b5f
sgobotta <santiago@camba.coop>
2019-08-25 04:25:01 -0300

    Admin composer, creates a functional wrapper:
    +   assigns default modules for the store into it's props
    +   renders an Admin component with new props

---
+++ src/components/Admin/src/Composer.vue
@@ -0,0 +1,17 @@
+<script>
+import Admin from './Admin'
+
+import createAuthModule from '@va-auth/store'
+
+export default {
+  functional: true,
+  render(createElement, context) {
+    const { props: { authProvider, options: _options = {} } } = context
+    const options = Object.assign({}, {
+      authModule: createAuthModule({ client: authProvider })
+    }, _options)
+    const props = Object.assign({}, context.props, { options })
+    return createElement(Admin, { props }, context.slots().default)
+  }
+}
+</script>
