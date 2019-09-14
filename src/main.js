46475b97f036454ca01d80864a957f07c36155a6
sgobotta <santiago@camba.coop>
2019-09-14 15:51:47 -0300

    Main.js: refactors plugins instantiation

--- src/main.js
+++ src/main.js
@@ -2,18 +2,22 @@ import '@babel/polyfill'
 import 'material-design-icons-iconfont/dist/material-design-icons.css'
 import './styles/main.sass'
 import './assets/fonts/Montserrat/Montserrat.css'
-import Vue from "vue"
-import Vuex from "vuex"
-import VueRouter from "vue-router"
-import vuetify from "@plugins/vuetify"
+import Vue from 'vue'
+import Vuex from 'vuex'
+import VueRouter from 'vue-router'
+import vueChartist from '@plugins/chartist'
+import vuetify from '@plugins/vuetify'
+import '@plugins/material'
 
 Vue.config.productionTip = false
+
+Vue.use(vueChartist)
 
 Vue.use(VueRouter)
 const routes = [{}]
 const router = new VueRouter(routes)
 
-import App from "@demo/App.vue"
+import App from '@demo/App.vue'
 
 Vue.use(Vuex)
 const store = new Vuex.Store({})
@@ -23,7 +27,7 @@ const app = new Vue({
   store,
   vuetify,
   render: h => h(App)
-}).$mount("#app")
+}).$mount('#app')
 
 if (window.Cypress) {
   // exposes the app, only available during E2E tests
