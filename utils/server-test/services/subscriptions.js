a7350002990dccbfc67c5b936ca081e19f206f1b
sgobotta <santiago@camba.coop>
2019-10-26 12:42:02 -0300

    Subscriptions: updates getMany endpoint

--- utils/server-test/services/subscriptions.js
+++ utils/server-test/services/subscriptions.js
@@ -1,3 +1,5 @@
+const moment = require('moment')
+
 module.exports = function(app) {
   const subscriptions = [
     {
@@ -5,82 +7,104 @@ module.exports = function(app) {
       month: 'January',
       shortMonth: 'Ja',
       subscriptions: 913,
+      updatedAt: '2019-10-25T23:43:03.729Z',
     },
     {
       id: 2,
       month: 'February',
       shortMonth: 'Fe',
       subscriptions: 1020,
+      updatedAt: '2019-10-25T23:18:07.702Z',
     },
     {
       id: 3,
       month: 'March',
       shortMonth: 'Ma',
       subscriptions: 980,
+      updatedAt: '2019-10-25T15:47:53.627Z',
     },
     {
-      id: 3,
+      id: 4,
       month: 'April',
       shortMonth: 'Ap',
       subscriptions: 853,
+      updatedAt: '2019-10-25T22:55:19.225Z',
     },
     {
-      id: 3,
+      id: 5,
       month: 'May',
       shortMonth: 'Ma',
       subscriptions: 795,
+      updatedAt: '2019-10-26T13:14:53.367Z',
     },
     {
-      id: 3,
+      id: 6,
       month: 'June',
       shortMonth: 'Ju',
       subscriptions: 750,
+      updatedAt: '2019-10-26T08:50:04.573Z',
     },
     {
-      id: 3,
+      id: 7,
       month: 'July',
       shortMonth: 'Ju',
       subscriptions: 613,
+      updatedAt: '2019-10-25T19:55:46.293Z',
     },
     {
-      id: 3,
+      id: 8,
       month: 'August',
       shortMonth: 'Au',
       subscriptions: 740,
+      updatedAt: '2019-10-25T20:08:57.991Z',
     },
     {
-      id: 3,
+      id: 9,
       month: 'September',
       shortMonth: 'Se',
       subscriptions: 780,
+      updatedAt: '2019-10-26T06:47:55.558Z',
     },
     {
-      id: 3,
+      id: 10,
       month: 'October',
       shortMonth: 'Oc',
       subscriptions: 890,
+      updatedAt: '2019-10-26T13:07:44.519Z',
     },
     {
-      id: 3,
+      id: 11,
       month: 'November',
       shortMonth: 'No',
       subscriptions: 420,
+      updatedAt: '2019-10-26T07:30:39.037Z',
     },
     {
-      id: 3,
+      id: 12,
       month: 'December',
       shortMonth: 'De',
       subscriptions: 627,
+      updatedAt: '2019-10-26T02:54:10.989Z',
     },
   ]
 
   app.get('/api/subscriptions', (req, res) => {
-    res.json(subscriptions)
+    const sortedSubscriptions = subscriptions.sort((a, b) =>
+      moment(a.updatedAt).isAfter(moment(b.updatedAt))
+    )
+    const { updatedAt } = sortedSubscriptions[sortedSubscriptions.length - 1]
+    const response = {
+      updatedAt,
+      subscriptions,
+    }
+    res.json(response)
   })
 
   app.get('/api/subscriptions/:id', (req, res) => {
-    const magazine = subscriptions.find(a => a.id.toString() === req.params.id)
-    const index = subscriptions.indexOf(magazine)
+    const subscription = subscriptions.find(
+      a => a.id.toString() === req.params.id
+    )
+    const index = subscriptions.indexOf(subscription)
 
     res.json(subscriptions[index])
   })
@@ -122,8 +146,10 @@ module.exports = function(app) {
   })
 
   app.delete('/api/subscriptions/:id', (req, res) => {
-    const magazine = subscriptions.find(a => a.id.toString() === req.params.id)
-    const index = subscriptions.indexOf(magazine)
+    const subscription = subscriptions.find(
+      a => a.id.toString() === req.params.id
+    )
+    const index = subscriptions.indexOf(subscription)
 
     if (index >= 0) subscriptions.splice(index, 1)
 
@@ -134,7 +160,7 @@ module.exports = function(app) {
     const id = subscriptions[subscriptions.length - 1].id + 1
     const { body } = req
 
-    const magazine = {
+    const subscription = {
       id,
       name: body.name,
       editorial: body.editorial,
@@ -142,8 +168,8 @@ module.exports = function(app) {
       publisher: body.publisher,
     }
 
-    subscriptions.push(magazine)
+    subscriptions.push(subscription)
 
-    res.status(201).send(magazine)
+    res.status(201).send(subscription)
   })
 }
