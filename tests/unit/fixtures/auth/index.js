e86ef7c6c4185b777db50f07d6bc74286337b288
sgobotta <santiago@camba.coop>
2019-11-16 20:04:40 -0300

    Unit tests, auth: updates fixtures
     updates unit tests lib: generalises findButtonByName function

--- tests/unit/fixtures/auth/index.js
+++ tests/unit/fixtures/auth/index.js
@@ -1,18 +1,28 @@
 import defaults from '@components/Layouts/src/AuthLayout/defaults'
 
-export default {
-  props: {
-    authFormTitle: defaults().props.authFormTitle,
-    authFooter: defaults().props.authFooter,
-    authMainContent: defaults().props.authMainContent,
-    usernameRules: defaults().props.usernameRules,
-    passwordRules: defaults().props.passwordRules,
-    va: {
-      login: () => {
-        return {
-          then: () => {},
-        }
+export default state => {
+  const { username: userCredential, password: passwordCredential } = state
+
+  return {
+    props: {
+      authFormTitle: defaults().props.authFormTitle,
+      authFooter: defaults().props.authFooter,
+      authMainContent: defaults().props.authMainContent,
+      usernameRules: defaults().props.usernameRules,
+      passwordRules: defaults().props.passwordRules,
+      va: {
+        login: (username, password) => {
+          return username === userCredential && password === passwordCredential
+            ? { then: () => {} }
+            : new Promise(resolve => {
+                resolve({
+                  response: {
+                    status: 401,
+                  },
+                })
+              })
+        },
       },
     },
-  },
+  }
 }
