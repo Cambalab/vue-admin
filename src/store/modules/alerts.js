7d28b2f2076d20f1bcb63f2dcb091f308522acee
sgobotta <santiago@camba.coop>
2020-01-14 21:53:36 -0300

    Alerts store: updates the module state to a snackbar object
    + updates mutations and getters

--- src/store/modules/alerts.js
+++ src/store/modules/alerts.js
@@ -4,23 +4,31 @@ export const Types = {
   ALERTS_HIDE_SNACKBAR: 'ALERTS_HIDE_SNACKBAR',
   ALERTS_SHOW_SNACKBAR: 'ALERTS_SHOW_SNACKBAR',
 
-  ALERTS_GET_STATUS: 'ALERTS_GET_STATUS',
+  ALERTS_GET_SNACKBAR_STATUS: 'ALERTS_GET_SNACKBAR_STATUS',
 }
 
 export default {
   namespaced: true,
   state: {
-    isVisible: false,
+    snackbar: {
+      color: '',
+      isVisible: false,
+      text: '',
+    },
   },
   mutations: {
     [Types.ALERTS_HIDE_SNACKBAR](state) {
+      state.snackbar.color = ''
+      state.snackbar.text = ''
       state.isVisible = false
     },
-    [Types.ALERTS_SHOW_SNACKBAR](state) {
-      state.isVisible = true
+    [Types.ALERTS_SHOW_SNACKBAR](state, { color, text }) {
+      state.snackbar.color = color
+      state.snackbar.text = text
+      state.snackbar.isVisible = true
     },
   },
   getters: {
-    [Types.ALERTS_GET_STATUS]: state => state.isVisible,
+    [Types.ALERTS_GET_SNACKBAR_STATUS]: state => state.snackbar,
   },
 }
