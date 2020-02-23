5d1402620ca01c92f26e43cbbb39a3a74aba46f7
sgobotta <santiago@camba.coop>
2020-02-23 00:11:23 -0300

    Deletes unusued wrappers components

--- src/components/Wrappers/TextInput/index.js
+++
@@ -1,7 +0,0 @@
-import TextInputWrapper from './TextInputWrapper'
-
-TextInputWrapper.install = function(Vue) {
-  Vue.component(TextInputWrapper.name, TextInputWrapper)
-}
-
-export default TextInputWrapper
--- src/components/Wrappers/index.js
+++
@@ -1,5 +0,0 @@
-import ButtonWrapper from './Button'
-import SimpleFormWrapper from './SimpleForm'
-import TextInputWrapper from './TextInput'
-
-export { ButtonWrapper, SimpleFormWrapper, TextInputWrapper }
