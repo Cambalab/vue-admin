36bfb052f94d3a6823aff8c865e8f627771a3b03
sgobotta <santiago@camba.coop>
2020-02-24 02:56:41 -0300

    Unit tests: Show. Implements tests for the action buttons, the title and the fields props.

--- tests/unit/specs/components/actions/show.spec.js
+++ tests/unit/specs/components/actions/show.spec.js
@@ -4,11 +4,12 @@ import Vuex from 'vuex'
 import Vuetify from 'vuetify'
 import Show from '@components/Actions/Show/Show'
 import UI_NAMES from '@constants/ui.element.names'
-// import UI_CONTENT from '@constants/ui.content.default'
+import UI_CONTENT from '@constants/ui.content.default'
 import { Types as CrudTypes } from '@store/modules/crud'
 import requestsModule from '@store/modules/requests'
 import showUtils from '@store/utils/show.utils'
 import Factory from '@unit/factory'
+import { showFixture } from '@unit/fixtures/actions'
 import { findElemByName } from '@unit/lib/utils/wrapper'
 import { shallowMount, createLocalVue } from '@vue/test-utils'
 
@@ -26,15 +27,14 @@ describe('Show.vue', () => {
   // mocks
   let mockedRouterPushParams
   let mockedRouter
+  let mockedState
   let mockedStore
   let mocks
-  // stubs
-  // let stubs
   // spies
-  // let routerSpy
   let storeSpy
   // props
   let propsData
+  let resourceName
 
   function mountSubject() {
     subjectWrapper = shallowMount(Show, {
@@ -47,23 +47,90 @@ describe('Show.vue', () => {
     })
   }
 
+  function findActionsContainer() {
+    const actionsContainerName = UI_NAMES.RESOURCE_VIEW_ACTIONS_CONTAINER.with({
+      resourceName,
+      view,
+    })
+    return findElemByName({
+      wrapper: subjectWrapper,
+      el: 'v-toolbar-stub',
+      name: actionsContainerName,
+    })
+  }
+
+  function findTitleCard() {
+    const titleName = UI_NAMES.RESOURCE_VIEW_CONTAINER_TITLE.with({
+      resourceName: propsData.resourceName,
+      view,
+    })
+    return findElemByName({
+      wrapper: findActionsContainer(),
+      el: 'v-card-title-stub',
+      name: titleName,
+    })
+  }
+
+  function findDeleteButton() {
+    const deleteButtonName = UI_NAMES.RESOURCE_VIEW_ACTIONS_DELETE_BUTTON.with({
+      resourceName,
+      view,
+    })
+    return findElemByName({
+      wrapper: findActionsContainer(),
+      el: 'deletebutton-stub',
+      name: deleteButtonName,
+    })
+  }
+
+  function findEditButton() {
+    const editButtonName = UI_NAMES.RESOURCE_VIEW_ACTIONS_EDIT_BUTTON.with({
+      resourceName,
+      view,
+    })
+    return findElemByName({
+      wrapper: findActionsContainer(),
+      el: 'editbutton-stub',
+      name: editButtonName,
+    })
+  }
+
+  function findFieldsContainer() {
+    const fieldsContainerName = UI_NAMES.RESOURCE_VIEW_CONTAINER_FIELDS.with({
+      resourceName,
+      view,
+    })
+    return findElemByName({
+      wrapper: subjectWrapper,
+      el: 'v-card-text-stub',
+      name: fieldsContainerName,
+    })
+  }
+
+  function findField(field) {
+    const fieldName = UI_NAMES.RESOURCE_VIEW_CONTAINER_FIELD.with({
+      resourceName,
+      view,
+      field,
+    })
+    return findElemByName({
+      wrapper: findFieldsContainer(),
+      el: 'simpletext-stub',
+      name: fieldName,
+    })
+  }
+
   beforeEach(() => {
     // mocks
+    mockedRouterPushParams = { id: 1 }
+    mockedState = showFixture().state()
     mockedStore = new Vuex.Store({
       modules: {
         requests: requestsModule,
       },
     })
     const { namespace, module } = Factory.createCrudModule({
-      state: {
-        entities: {
-          '1': {
-            resourceName: 'the name of a resource',
-            fields: 'children nodes used in the Show component',
-            va: 'an object containing methods',
-          },
-        },
-      },
+      state: mockedState,
       store: mockedStore,
     })
     mockedStore.registerModule(namespace, module)
@@ -74,39 +141,13 @@ describe('Show.vue', () => {
     }
 
     // props
-    const resourceName = 'resource'
+    resourceName = 'resource'
     const utils = showUtils({
       resourceName,
       router: mockedRouter,
       store: mockedStore,
     })
-    propsData = {
-      fields: [
-        {
-          placeHolder: 'a resource name',
-          label: 'resourceName',
-          type: 'h2',
-          tag: 'SimpleText',
-        },
-        {
-          placeHolder: 'a fields array',
-          label: 'fields',
-          type: 'h3',
-          tag: 'SimpleText',
-        },
-        {
-          placeHolder: 'a va object',
-          label: 'va',
-          type: 'p',
-          tag: 'SimpleText',
-        },
-      ],
-      resourceName,
-      title: 'My Resource',
-      va: {
-        ...utils,
-      },
-    }
+    propsData = showFixture().props({ resourceName, utils })
 
     const routes = [
       {
@@ -117,16 +158,12 @@ describe('Show.vue', () => {
       },
     ]
     mockedRouter.addRoutes(routes)
-    mockedRouterPushParams = { id: 1 }
     mockedRouter.push({
       name: 'resource/show',
       params: { ...mockedRouterPushParams },
     })
 
     // spies
-    // routerSpy = {
-    //   addRoutes: jest.spyOn(mocks.router, 'addRoutes'),
-    // }
     storeSpy = {
       dispatch: jest.spyOn(mocks.$store, 'dispatch'),
     }
@@ -140,6 +177,7 @@ describe('Show.vue', () => {
     mountSubject()
 
     const props = subjectWrapper.props()
+    expect(props.title).toMatch(propsData.title)
     expect(props.fields).toMatchObject(propsData.fields)
     expect(props.resourceName).toMatch(propsData.resourceName)
     expect(props.va).toMatchObject(propsData.va)
@@ -166,18 +204,22 @@ describe('Show.vue', () => {
     expect(subjectWrapper.vm.resourceShow).toMatchObject(entityState)
   })
 
+  it('should use a default title', () => {
+    delete propsData.title
+    mountSubject()
+
+    const titleName = UI_CONTENT.RESOURCE_VIEW_TITLE.with({ resourceName })
+    const { title: dataTitle } = subjectWrapper.vm.$data.content
+    const titleCard = findTitleCard()
+
+    expect(dataTitle).toMatch(titleName)
+    expect(titleCard.text()).toMatch(titleName)
+  })
+
   it('should contain an actions toolbar', () => {
     mountSubject()
 
-    const actionsContainerName = UI_NAMES.RESOURCE_VIEW_ACTIONS_CONTAINER.with({
-      resourceName: propsData.resourceName,
-      view,
-    })
-    const actionsContainer = findElemByName({
-      wrapper: subjectWrapper,
-      el: 'v-toolbar-stub',
-      name: actionsContainerName,
-    })
+    const actionsContainer = findActionsContainer()
 
     expect(actionsContainer.exists()).toBe(true)
   })
@@ -185,25 +227,38 @@ describe('Show.vue', () => {
   it('should render an actions toolbar with a title', () => {
     mountSubject()
 
-    const actionsContainerName = UI_NAMES.RESOURCE_VIEW_ACTIONS_CONTAINER.with({
-      resourceName: propsData.resourceName,
-      view,
-    })
-    const actionsContainer = findElemByName({
-      wrapper: subjectWrapper,
-      el: 'v-toolbar-stub',
-      name: actionsContainerName,
-    })
-    const titleName = UI_NAMES.RESOURCE_VIEW_CONTAINER_TITLE.with({
-      resourceName: propsData.resourceName,
-      view,
-    })
-    // const title = findRef({ wrapper: subjectWrapper, ref: titleName})
-    const titleCard = findElemByName({
-      wrapper: actionsContainer,
-      el: 'v-card-title-stub',
-      name: titleName,
-    })
+    const titleCard = findTitleCard()
     expect(titleCard.text()).toMatch(propsData.title)
+  })
+
+  it('should render an actions toolbar with an edit button', () => {
+    mountSubject()
+
+    const editButton = findEditButton()
+
+    expect(editButton.exists()).toBe(true)
+  })
+
+  it('should render an actions toolbar with a delete button', () => {
+    mountSubject()
+
+    const deleteButton = findDeleteButton()
+
+    expect(deleteButton.exists()).toBe(true)
+  })
+
+  it('should render fields', () => {
+    mountSubject()
+
+    const { fields } = propsData
+    const state = mockedState.entities[mockedRouterPushParams.id]
+
+    fields.forEach(f => {
+      const field = findField(f.label)
+      const { type, value } = field.props()
+      expect(field.exists()).toBe(true)
+      expect(type).toMatch(f.type)
+      expect(value).toMatch(state[f.label])
+    })
   })
 })
