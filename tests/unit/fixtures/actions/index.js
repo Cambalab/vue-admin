89a9a57c8a0bd105e721549c1489d9a9dc2f2a96
sgobotta <santiago@camba.coop>
2020-03-02 00:58:36 -0300

    Unit fixtures: creates a fixture for the list component test

--- tests/unit/fixtures/actions/index.js
+++ tests/unit/fixtures/actions/index.js
@@ -88,3 +88,65 @@ export const createFixture = () => {
     state,
   }
 }
+
+export const listFixture = () => {
+  const props = ({ resourceName = 'resource', utils }) => {
+    return {
+      hasShow: true,
+      hasCreate: true,
+      hasEdit: true,
+      fields: [
+        {
+          placeHolder: 'a resource name',
+          label: 'resourceName',
+          tag: 'SimpleText',
+        },
+        {
+          placeHolder: 'a fields array',
+          label: 'fields',
+          tag: 'SimpleText',
+        },
+        {
+          placeHolder: 'a va object',
+          label: 'va',
+          tag: 'SimpleText',
+        },
+      ],
+      resourceIdName: 'id',
+      resourceName,
+      title: 'My Resource',
+      va: {
+        ...utils,
+      },
+    }
+  }
+  const state = () => {
+    return {
+      list: [1, 2, 3],
+      entities: {
+        '1': {
+          id: 1,
+          resourceName: 'the name of a resource',
+          fields: 'children nodes used in the Show component',
+          va: 'an object containing methods',
+        },
+        '2': {
+          id: 2,
+          resourceName: 'the name of another resource',
+          fields: 'children nodes used in the Create component',
+          va: 'an object containing methods',
+        },
+        '3': {
+          id: 3,
+          resourceName: 'the name of a resource',
+          fields: 'children nodes used in the Edit component',
+          va: 'an object containing methods',
+        },
+      },
+    }
+  }
+  return {
+    props,
+    state,
+  }
+}
